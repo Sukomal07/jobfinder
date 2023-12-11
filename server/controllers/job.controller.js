79f6ce6265bf5ffc279dbc477c023da628b18928
Sukomal07 <hypersukomal@gmail.com>
2023-12-11 23:16:36 +0530

    added filter search and edit job and get jobs

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -52,3 +52,127 @@ export const createJobs = async (req, res, next) => {
         return next(createError(error.message))
     }
 }
+export const editJob = async (req, res, next) => {
+    try {
+        const { id } = req.user;
+        const { jobId } = req.params;
+
+        const { skills: skillsString, ...updateData } = req.body;
+
+        const existingJob = await Job.findOne({ _id: jobId, createdBy: id });
+
+        if (!existingJob) {
+            return next(createError(400, 'No job found or you are not allowed'));
+        }
+
+        const skills = skillsString.split(',').map(skill => skill.trim());
+
+        if (skills.length < 3) {
+            return next(createError(400, 'At least 3 skills are required'));
+        }
+
+        const update = {
+            skills,
+            updateData
+        }
+
+        const updateJob = await Job.findByIdAndUpdate(
+            jobId,
+            { $set: update },
+            { new: true }
+        );
+
+        try {
+            await updateJob.validate();
+        } catch (error) {
+            const validationErrors = [];
+            for (const key in error.errors) {
+                validationErrors.push(`${key}: ${error.errors[key].message}`);
+            }
+            return next(createError(400, validationErrors.join(', ')));
+        }
+
+        await updateJob.save();
+
+        res.status(201).json({
+            success: true,
+            message: 'Job updated successfully',
+            updateJob
+        });
+    } catch (error) {
+        return next(createError(500, error.message));
+    }
+}
+
+export const getAllJobs = async (req, res, next) => {
+    try {
+        const allJobs = await Job.find();
+
+        res.status(200).json({
+            success: true,
+            jobs: allJobs,
+        });
+    } catch (error) {
+        return next(createError(500, error.message));
+    }
+}
+
+export const getJobsWithFilters = async (req, res, next) => {
+    try {
+        const { text, skills } = req.query;
+
+        let filter = {};
+
+        if (text) {
+            filter = {
+                $or: [
+                    { companyName: { $regex: new RegExp(text, 'i') } },
+                    { position: { $regex: new RegExp(text, 'i') } }
+                ],
+            };
+        }
+
+        if (skills) {
+            const skillsArray = skills.split(',').map(skill => skill.trim());
+
+            const skillRegexArray = skillsArray.map(skill => new RegExp(skill, 'i'));
+
+            filter.skills = { $in: skillRegexArray };
+        }
+
+        const jobs = await Job.find(filter);
+
+        if (jobs.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: 'No jobs found with the specified filters.'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            jobs
+        });
+    } catch (error) {
+        return next(createError(500, error.message));
+    }
+}
+
+export const getJobById = async (req, res, next) => {
+    try {
+        const { jobId } = req.params;
+
+        const job = await Job.findById(jobId);
+
+        if (!job) {
+            return next(createError(404, 'Job not found'))
+        }
+
+        res.status(200).json({
+            success: true,
+            job
+        });
+    } catch (error) {
+        return next(createError(500, error.message));
+    }
+}
--- server/routes/job.routes.js
+++ server/routes/job.routes.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { createJobs } from '../controllers/job.controller.js'
+import { createJobs, editJob, getAllJobs, getJobById, getJobsWithFilters } from '../controllers/job.controller.js'
 import { isLoggedIn } from '../middleware/auth.middleware.js'
 const router = express.Router()
 
 router.post("/createjob", isLoggedIn, createJobs)
+router.put("/:jobId/editjob", isLoggedIn, editJob)
+router.get("/alljobs", getAllJobs)
+router.get("/jobs", getJobsWithFilters)
+router.get("/:jobId/viewjob", getJobById)
 
 export default router
