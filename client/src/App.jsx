fa46453f6c7bdd679e5a684593b1db97662c2f3d
Sukomal07 <hypersukomal@gmail.com>
2023-12-13 22:10:51 +0530

    create files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
+import './styles/App.css'
+import { Routes, Route } from 'react-router-dom'
 
+import NotFound from './pages/NotFound'
+import Home from './pages/Home'
+import LogIn from './pages/LogIn'
+import Signup from './pages/Signup'
 
 function App() {
-
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <Routes>
+        <Route path='*' element={<NotFound />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <Toaster />
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </React.StrictMode>,
 )
