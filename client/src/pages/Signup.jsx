ca306e10f38f26aaf00f6d8fb15d23433ce22b2f
Sukomal07 <hypersukomal@gmail.com>
2023-12-14 21:12:35 +0530

    signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,34 @@
+import { Link } from 'react-router-dom'
+import '../styles/Signup.css'
+import loginImg from '../assets/loginImg.png'
 
 function Signup() {
     return (
-        <div>
-            <h1>signup</h1>
+        <div className='signup_container'>
+            <div className='left_container'>
+                <form className='signup_form'>
+                    <h1>Create an account</h1>
+                    <p>Your personal job finder is here</p>
+                    <div className='inputs'>
+                        <input type="text" name='name' id='name' placeholder='Name' />
+                        <input type="email" name="email" id="email" placeholder='Email' />
+                        <input type="tel" name="mobile" id="mobile" placeholder='Mobile' />
+                        <input type="password" name="password" id="password" placeholder='Password' />
+                    </div>
+                    <div className='privacy'>
+                        <input type="checkbox" name="checkbox" id="checkbox" />
+                        <p>By creating an account, I agree to our terms of use and privacy policy</p>
+                    </div>
+                    <button type="submit">Create Account</button>
+                    <p>Already have an acount ? <Link to={'/login'}>Sign in</Link>  </p>
+                </form>
+            </div>
+            <div className='right_container'>
+                <div className='image'>
+                    <img src={loginImg} alt="photo" />
+                    <p>Your Personal Job Finder</p>
+                </div>
+            </div>
         </div>
     )
 }
